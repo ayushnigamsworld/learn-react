ae77e11ff98fc0c4104ad275c1040d9c2afa5547
srajpal <srajpal@3clogic.com>
2021-08-02 20:24:31 +0530

    Merge branch 'main' of https://github.com/ayushnigamsworld/learn-react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,74 +1,102 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Tasks from "./components/Tasks";
-import {useState} from "react";
-import AddTask from "./components/AddTask";
+import "./App.css";
+// import { render } from '@testing-library/react';
+import { Component, Fragment } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/Users/Users";
+import User from "./components/Users/User";
+import Search from "./components/Users/Search";
+import Alert from "./components/layout/Alert";
+import axios from "axios";
+import About from "./components/pages/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+class App extends Component {
+  state = {
+    users: [],
+    isLoading: false,
+    alert: null,
+    user: {},
+  };
 
-function App() {
-
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            name: 'Jo',
-            reminder: false
-        },
-        {
-            id: 2,
-            name: 'Very Jo',
-            reminder: false
-        },
-        {
-            id: 3,
-            name: 'Very Very Jo',
-            reminder: false
-        }
-    ]);
-
-    const onDelete = (id) => {
-        // alert(`Deleted :: ${id}`)kh;
-        setTasks(tasks.filter( e => e.id !== id));
-    };
-
-    const toggleReminder = (id) => {
-        // alert(`Double clicked :: ${id}`);
-        setTasks(
-            tasks.map( e => e.id === id ?
-                { ...e, reminder: !e.reminder } : e ));
-    };
-
-    const addTask = ({ text, day, reminder }) => {
-        console.log(text, day, reminder);
-        const id = Math.floor(Math.random() * 10000);
-        setTasks([...tasks, { id, reminder, day, name: text }]);
-    };
-
-    const [showAddTask, setShowAddTask] = useState(false);
-
-    const toggleAddTaskBtn = () => {
-      setShowAddTask(!showAddTask);
-    };
-
-    return (
-        <div className='container'>
-            <Header toggleAddTaskBtn={toggleAddTaskBtn} title='Hola'/>
-            {
-                showAddTask && <AddTask onAdd={addTask} />
-            }
-            {
-                tasks.length > 0 ?
-                    (<Tasks tasks={tasks} onDelete={onDelete} toggleReminder={toggleReminder}/>)
-                    :
-                    ('No tasks')
-            }
-        </div>
+  searchUsers = async (text) => {
+    this.setState({ isLoading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=510115bfbfe9a19aa2fd&client_secret=1b2ffda8e0c38489adab57c8f38a6982d0fdb1e3`
     );
-}
+    this.setState({ isLoading: false, users: res.data.items });
+  };
 
-/*class App extends React.Component {
-  render() {
-    return <h1>Hello from Class</h1>
+  getUser = async (username) => {
+    this.setState({ isLoading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=510115bfbfe9a19aa2fd&client_secret=1b2ffda8e0c38489adab57c8f38a6982d0fdb1e3`
+    );
+    this.setState({ isLoading: false, user: res.data });
+  };
+  /*
+  async componentDidMount() {
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+    this.setState({ isLoading: true });
+    const res = await axios.get(
+      "https://api.github.com/users?client_id=510115bfbfe9a19aa2fd&client_secret=1b2ffda8e0c38489adab57c8f38a6982d0fdb1e3"
+    );
+    this.setState({ isLoading: false, users: res.data });
   }
-}*/
+  */
+  clearUsers = () => {
+    this.setState({ users: [], isLoading: false });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
+
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <Navbar title='Github Finder' icon='fab fa-github' />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={this.state.users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      isLoading={this.state.isLoading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={this.state.user}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
