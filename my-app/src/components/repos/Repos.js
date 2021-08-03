b241f3b83d43af2d222d2f3b49ced831ca4c7c51
srajpal <srajpal@3clogic.com>
2021-08-03 19:26:19 +0530

    commit

--- my-app/src/components/repos/Repos.js
+++ my-app/src/components/repos/Repos.js
@@ -2,7 +2,7 @@ import React from "react";
 import RepoItem from "./RepoItem";
 const Repos = (props) => {
   return props.repos.map((repo) => {
-    // <RepoItem repo={repo} key={repo.id} />;
+    <RepoItem repo={repo} key={repo.id} />;
     <p>hi</p>;
   });
 };
